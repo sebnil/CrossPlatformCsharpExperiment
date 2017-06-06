989aaaa9b404f095814febc8dc7658beea6eaa3d
Sebastian Nilsson <sebnil@gmail.com>
2017-06-06 20:32:28 +0200

    adds gtk project. tested on linux mint

---
+++ CrossPlatformCsharpExpermient.GTK/MainWindow.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Gtk;
+using CrossPlatformCsharpExperiment.Platform;
+
+public partial class MainWindow: Gtk.Window
+{
+	public MainWindow () : base (Gtk.WindowType.Toplevel)
+	{
+		Build ();
+	}
+
+	protected void OnDeleteEvent (object sender, DeleteEventArgs a)
+	{
+		Application.Quit ();
+		a.RetVal = true;
+	}
+
+	protected void buttonClicked (object sender, EventArgs e)
+	{
+		lblRandomInteger.Text = "Random number: " + RandomGenerator.RandomInteger ().ToString ("F0");
+	}
+}
